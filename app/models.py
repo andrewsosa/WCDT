c5ca7706bc52284cf9f057bc660a082913120368
andrewsosa001 <andrewsosa001@gmail.com>
2016-04-05 21:14:29 -0400

    Made the crawler

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from app import db
 class Headline(db.Document):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     text = db.StringField(max_length=255, required=True)
+    person = db.StringField(max_length=255, required=True)
 
     def __unicode__(self):
         return self.text
