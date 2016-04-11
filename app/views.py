59f1e156d447f9e8e28efd0653761c1f121454c0
= <=>
2016-04-11 10:58:58 -0400

    Fixed the stuff

--- app/views.py
+++ app/views.py
@@ -1,28 +1,38 @@
 from flask import render_template
 from app import app
 from app.models import *
+from app import db
 import datetime
-from babel.dates import format_datetime
+from dateutil.parser import parse
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    headlines = Headline.objects.order_by('created_at')
-    most_recent = headlines[0]
-    days_since = (datetime.datetime.now().date() - most_recent.created_at.date()).days
-    return render_template('index.html',
-                        days_since=str(days_since),
-                        headlines=headlines)
-
-
-
-@app.template_filter('datetime')
-def my_custom_format(value, format='MMMM d yyyy'):
     try:
-        return format_datetime(value, format)
-    except:
-        return format_datetime(value)
+        headlines = Headline.objects.order_by('created_at')
+        most_recent = headlines[0]
+        days_since = (datetime.datetime.now().date() - most_recent.created_at.date()).days
+        return render_template('index.html',
+                            days_since=str(days_since),
+                            headlines=headlines)
+    except Exception as e:
+        return e
+
+"""
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    date = parse(date)
+    native = date.replace(tzinfo=None)
+    format='%b %d, %Y'
+    return native.strftime(format)
+"""
 
 @app.route("/test")
 def test():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    #return "<h1 style='color:blue'>Hello Butts!</h1>"
+    #return str(Headline.objects.order_by('created_at'))
+    try:
+        return str(Headline.objects)
+    except Exception as e:
+        return e
